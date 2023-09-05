105086e09866237482906c83c8d3d72503b230c6
Lewisay7 <lewischong0726@gmail.com>
2023-09-05 20:28:29 +0800

    Readed in given data and baseline for cleaning

---
+++ src/clean_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+url= "TIKI-TOKI/data/ad_ranking_raw_data.xlsx"
+ad_data = pd.read_excel(url,sheet_name=0,header=1)
+ppl_data = pd.read_excel(url,sheet_name=1,header=1)
+
+ad_data.head()
+ad_data.ad_id.is_unique
+pd.unique(ad_data["queue_market"])
+
+
