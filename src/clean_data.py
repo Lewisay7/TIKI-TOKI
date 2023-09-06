ad4500c77add3ed280d1e14bcd4baa9bb8a8a4d1
looyinggene <142077477+looyinggene@users.noreply.github.com>
2023-09-06 13:15:14 +0800

    made by gene.

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 url= "TIKI-TOKI/data/ad_ranking_raw_data.xlsx"
 ad_data = pd.read_excel(url,sheet_name=0,header=1)
-ppl_data = pd.read_excel(url,sheet_name=1,header=1)
 
 ad_data.head()
 ad_data.ad_id.is_unique
