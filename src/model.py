31c1de5f3b7dbdfa54ae43b67aa8d29ca9464732
looyinggene <142077477+looyinggene@users.noreply.github.com>
2023-09-09 15:20:35 +0800

    mod cols standardised

--- src/model.py
+++ src/model.py
@@ -1,9 +1,24 @@
 import clean_datasets as clean
+import pandas as pd
 import numpy as np
 import sklearn as sk
 import matplotlib as plt
+from sklearn.preprocessing import StandardScaler
 
 ad = clean.ad_cleaned()
 mod = clean.mod_cleaned()
 ad
 mod
+
+#standardising data
+scaler = StandardScaler()
+standardised_cols = scaler.fit_transform(mod.loc[:,'productivity':'accuracy'])
+cols = ['productivity', 'utilisation_percentage','handling_time','accuracy']
+df = pd.DataFrame(standardised_cols,columns = cols)
+df.shape
+
+
+
+
+    
+
