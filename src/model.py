da07b81b63359676e8e03820e25df45c2fb2b258
SPACEYNESS909 <brandonlsl010911@gmail.com>
2023-09-09 18:19:04 +0800

    model scores

--- src/model.py
+++ src/model.py
@@ -8,10 +8,13 @@ import prince
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+
 
 ad = clean.ad_cleaned()
 mod = clean.mod_cleaned()
 ad
+mod.index = range(1,len(mod)+1)
 mod
 
 '''MODERATOR MODEL'''
@@ -33,13 +36,13 @@ mod.isnull().sum()
 #visualising relationships between features
 sns.set(style="whitegrid")
 sns.pairplot(mod.loc[:,'productivity':'accuracy'], kind='reg', diag_kind='kde')
-plt.show()
+# plt.show()
 
 #Correlation
 plt.figure(figsize=(8,6))
 sns.set(style="whitegrid")
 sns.heatmap(mod.loc[:,'productivity':'accuracy'].corr(method='pearson'), vmin=-.1, vmax=1,  annot=True, cmap='RdYlGn')
-plt.show()
+# plt.show()
 
 #variance explained by principal components
 sk_pca = PCA(n_components=4,random_state=234)
@@ -48,7 +51,7 @@ sk_pca
 dset2 = pd.DataFrame()
 dset2['pca'] = range(1,5)
 dset2['vari'] = pd.DataFrame(sk_pca.explained_variance_ratio_)
-plt.figure(figsize=(8,6))
+# plt.figure(figsize=(8,6))
 graph = sns.barplot(x='pca', y='vari', data=dset2)
 for p in graph.patches:
     graph.annotate('{:.2f}'.format(p.get_height()), (p.get_x()+0.2, p.get_height()),
@@ -56,27 +59,18 @@ for p in graph.patches:
                    color= 'black')
 plt.ylabel('Proportion', fontsize=18)
 plt.xlabel('Principal Component', fontsize=18)
-plt.show()
-#snjfdn
+# plt.show()
+mod2 = sk_pca.transform(df)
+scores = (mod2 * sk_pca.explained_variance_ratio_).sum(axis=1)
 
-
-wcss = []  # Within-Cluster Sum of Squares
-
-# Try K values from 1 to a reasonable maximum (e.g., 10)
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, random_state=234)
-    kmeans.fit(df[['productivity', 'utilisation_percentage', 'handling_time', 'accuracy']])
-    wcss.append(kmeans.inertia_)
-
-# Plot the Elbow Method graph to choose the optimal K
-plt.figure(figsize=(8, 6))
-plt.plot(range(1, 11), wcss, marker='o', linestyle='-', color='b')
-plt.title('Elbow Method for Optimal K')
-plt.xlabel('Number of Clusters (K)')
-plt.ylabel('Within-Cluster Sum of Squares (WCSS)')
-plt.grid(True)
-plt.show()
-
+scaler = MinMaxScaler()
+scores = scores.reshape(-1, 1)
+scores = scaler.fit_transform(scores)
+scores
+mod["scores"] = scores
+mod
+mod = mod.sort_values(by='scores', ascending= False)
+mod
 
 
 
