46a76b086e5f7aa47122d507e76e3c632cbe80d0
Lewisay7 <lewischong0726@gmail.com>
2023-09-09 12:45:41 +0800

    clean up unnecessary changes

--- src/model.py
+++ src/model.py
@@ -1,10 +1,4 @@
-import numpy as np
-import clean_datasets
-import sys 
-sys.path.append("src")
 import clean_datasets as clean
-from . import ad_cleaned, mod_cleaned
-import sklearn as sk
 
-clean.ad_cleaned()
-clean.mod_cleaned()
+ad = clean.ad_cleaned()
+mod = clean.mod_cleaned()
