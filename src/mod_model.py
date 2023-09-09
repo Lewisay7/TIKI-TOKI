e09a6ff672e86f287d2923fc4844257ff7e7ca92
looyinggene <142077477+looyinggene@users.noreply.github.com>
2023-09-09 19:11:41 +0800

    ad scores obtained

--- src/mod_model.py
+++ src/mod_model.py
@@ -9,36 +9,39 @@ from sklearn.decomposition import PCA
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 
-ad = clean.ad_cleaned()
 
-'''ADVERTISEMENT MODEL'''
+ad = clean.ad_cleaned()
+mod = clean.mod_cleaned()
+ad
+mod.index = range(1,len(mod)+1)
+mod
+
+'''MODERATOR MODEL'''
 #standardising data
 scaler = StandardScaler()
-cols = ['punish_num','ad_revenue','baseline_st','duration_since_start_time']
-ad['duration_since_start_time'] = ad['duration_since_start_time'].dt.days
-ad_1 = ad.drop(['latest_punish_begin_date','avg_ad_revenue','start_time'], axis = 1)
-standardised_cols = scaler.fit_transform(ad_1.loc[:,'punish_num':'duration_since_start_time'])
-standardised_cols
+standardised_cols = scaler.fit_transform(mod.loc[:,'productivity':'accuracy'])
+cols = ['productivity', 'utilisation_percentage','handling_time','accuracy']
 df = pd.DataFrame(standardised_cols,columns = cols)
 df.index = df.index + 1
 
-ad_1['punish_num'] = df['punish_num']
-ad_1['ad_revenue'] = df['ad_revenue']
-ad_1['baseline_st'] = df['baseline_st']
-ad_1['duration_since_start_time'] = df['duration_since_start_time']
-ad_1
-ad_1.isnull().sum()
+df.shape
+mod['productivity'] = df['productivity']
+mod['utilisation_percentage'] = df['utilisation_percentage']
+mod['handling_time'] = df['handling_time']
+mod['accuracy'] = df['accuracy']
+mod
+mod.isnull().sum()
 
 #visualising relationships between features
 sns.set(style="whitegrid")
-sns.pairplot(ad_1.loc[:,'punish_num':'duration_since_start_time'], kind='reg', diag_kind='kde')
-plt.show()
+sns.pairplot(mod.loc[:,'productivity':'accuracy'], kind='reg', diag_kind='kde')
+# plt.show()
 
 #Correlation
 plt.figure(figsize=(8,6))
 sns.set(style="whitegrid")
-sns.heatmap(ad_1.loc[:,'punish_num':'duration_since_start_time'].corr(method='pearson'), vmin=-.1, vmax=1,  annot=True, cmap='RdYlGn')
-plt.show()
+sns.heatmap(mod.loc[:,'productivity':'accuracy'].corr(method='pearson'), vmin=-.1, vmax=1,  annot=True, cmap='RdYlGn')
+# plt.show()
 
 #variance explained by principal components
 sk_pca = PCA(n_components=4,random_state=234)
@@ -47,7 +50,7 @@ sk_pca
 dset2 = pd.DataFrame()
 dset2['pca'] = range(1,5)
 dset2['vari'] = pd.DataFrame(sk_pca.explained_variance_ratio_)
-plt.figure(figsize=(8,6))
+# plt.figure(figsize=(8,6))
 graph = sns.barplot(x='pca', y='vari', data=dset2)
 for p in graph.patches:
     graph.annotate('{:.2f}'.format(p.get_height()), (p.get_x()+0.2, p.get_height()),
@@ -55,13 +58,13 @@ for p in graph.patches:
                    color= 'black')
 plt.ylabel('Proportion', fontsize=18)
 plt.xlabel('Principal Component', fontsize=18)
-plt.show()
+# plt.show()
 
 #transforming the data using pca
-ad2 = sk_pca.transform(df)
+mod2 = sk_pca.transform(df)
 
 #assigning the scores using weights of the proportion of explained ratio by the principal components
-scores = (ad2 * sk_pca.explained_variance_ratio_).sum(axis=1)
+scores = (mod2 * sk_pca.explained_variance_ratio_).sum(axis=1)
 
 #scaling the scores into the range of 0 and 1 
 scaler = MinMaxScaler()
@@ -69,19 +72,12 @@ scores = scores.reshape(-1, 1)
 scores = scaler.fit_transform(scores)
 scores
 
-#Assigning scores to the original advertisement datasets
-ad["scores"] = scores
-ad
+#Assigning scores to the advertisement
+mod["scores"] = scores
+mod
 #sorting the ads based on scores
-ad = ad.sort_values(by='scores', ascending= False)
-ad
-
-
-
-
-
-
-
+mod = mod.sort_values(by='scores', ascending= False)
+mod
 
 
 
